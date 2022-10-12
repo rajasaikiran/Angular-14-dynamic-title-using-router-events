15c9edff93c44ee0c20ae7ec224dd75c36d60668
Rajasaikiran Vemula <rvemula@ariqt.com>
2022-10-12 14:36:17 +0530

    title

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,35 @@
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent,
+    data: {
+      title: 'home-title',
+    },
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
+    data: {
+      title: 'about-title',
+    },
+  },
+  {
+    path: 'contact',
+    component: ContactComponent,
+    data: {
+      title: 'contact-title',
+    },
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'route-title';
+
+  constructor(
+    private titleService: Title,
+    private router: Router,
+    private activePage: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    this.changeTitle();
+  }
+
+  changeTitle() {
+    this.router.events.subscribe((event) => {
+      switch (true) {
+        case event instanceof NavigationEnd:
+          let child = this.activePage.firstChild!;
+          // this.titleService.setTitle(child.snapshot.data.title);
+          this.titleService.setTitle(child.snapshot.data['title']);
+          break;
+
+        default:
+          break;
+      }
+    });
+  }
 }
